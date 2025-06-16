40fa7e0706cb15e9619f38346cece4a4fb1a2b69
Svyatoslav <slikit@mail.ru>
2025-06-16 23:22:05 +0300

    3-events: выполнил 3-е домашнее задание

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <script setup>
-import Button from "./components/Button.vue";
+import Score from "./components/Score.vue";
+import Card from "./components/Card.vue";
+
+const data = {
+  label: "100",
+};
 </script>
 
 <template>
+  <header class="header">
+    <div class="header__wrapper">
+      <span class="header__caption">ЗАПОМНИ СЛОВО</span>
+      <Score v-bind="data" />
+    </div>
+  </header>
   <main class="main">
-    <Button> Начать игру </Button>
+    <Card />
   </main>
 </template>
 
 <style scoped>
-  .main {
-    display: grid;
-    place-items: center;
-    min-height: 100vh;
-  }
+.header__wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 37px 62px;
+}
+
+.header__caption {
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 700;
+  letter-spacing: 0.12em;
+}
+.main {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  min-height: calc(100vh - 122px);
+  padding: 62px;
+}
 </style>
